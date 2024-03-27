b4eafd5ba98bf0db4d37ab37baaaa98fbf129d26
Franco Reyes <franco.javier.reyes@gmail.com>
2024-03-27 14:38:14 +1100

    Add CI/CD

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-export const HASURAGRES_URL = `http://${process.env.HASURAGRES_HOST}:${process.env.HASURAGRES_PORT}`;
+export const HASURAGRES_URL = process.env.HASURAGRES_URL;
 console.log("Url to hit: " + HASURAGRES_URL);
 
 export const HASURAGRES_API_KEY = process.env.HASURAGRES_API_KEY;
